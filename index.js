073b582be0573da88735f43366bee8ea14cf1835
Spatulox <M78stormtrooper@laposte.net>
2025-04-07 21:50:54 +0200

    add basic file

---
+++ index.js
@@ -0,0 +1,55 @@
+//Librairies
+import Discord from 'discord.js'
+// Files
+import config from './config.json' with { type: 'json' };
+
+// functions
+import {log, recapBotsErrors} from './utils/functions.js'
+import { checkInternetCo } from './utils/checkInternetCo.js'
+import { executeSlashCommand } from './commands/executeCommand.js';
+import { executeModalSubmit } from "./form/executeModalSubmit.js";
+import { loginBot, setActivity } from './utils/login.js';
+import { client, initOwner } from './client.js';
+
+function main(){
+
+	process.env.YTDL_NO_UPDATE = '1';
+
+	log('INFO : ----------------------------------------------------')
+	log('INFO : Starting Program');
+
+	checkInternetCo()
+    .then(() => {
+
+		log(`INFO : Using discord.js version: ${Discord.version}`);
+		log('INFO : Creating Client')
+		log('INFO : Trying to connect to Discord Servers')
+		let tmp = loginBot(client)
+		if (tmp === "Token error")
+		{
+			log('INFO : Stopping program')
+			process.exit()
+		}
+
+		client.on('ready', async () => {
+			log(`INFO : ${client.user.username} has logged in, waiting...`)
+			setActivity(client)
+			initOwner()
+
+			recapBotsErrors(client, config)
+			// Deploy Commands
+			//await deployCommand(client)
+		});
+
+		client.on('interactionCreate', async (interaction) => {
+			executeSlashCommand(interaction, client)
+		  });
+
+		client.on('interactionCreate', async interaction => {
+			executeModalSubmit(interaction, client)
+		});
+	});
+}
+
+main()
+
