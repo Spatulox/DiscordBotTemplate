5cd7bc3aab6dcb1ea84747bac107df964788b9ed
Spatulox <M78stormtrooper@laposte.net>
2026-02-03 10:35:55 +0100

    Initializes Discord client
    Sets up the Discord client with necessary gateway intents and partials.
    
    This initialization ensures the bot can connect to Discord and
    interact with guilds, members, messages, and reactions. Removes old
    implementation of client to start fresh.

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,7 +1,4 @@
-import { Client, GatewayIntentBits, Partials, User} from 'discord.js';
-import { searchClientChannel } from './guilds/channels.js';
-import { log } from './log.js';
-import config from '../config.js';
+import { Client, GatewayIntentBits, Partials} from 'discord.js';
 
 export const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
@@ -14,36 +11,3 @@ export const client = new Client({ intents: [
     ],
     partials: [Partials.Channel]
 });
-
-
-
-export let owner: User | null
-
-export async function initOwner(): Promise<boolean>{
-
-    if(config.sendToOwnerOrChannel === "0"){
-        try{
-            owner = await client.users.fetch(config.owner);
-            owner.send('Bot online');
-            return true
-        } catch (e){
-            owner = null
-            log(`ERROR : Something went wrong when searching for the owner : ${e}`)
-            return false
-        }
-    }
-    else if(config.sendToOwnerOrChannel === "1"){
-        try{
-            const channelLogin = await searchClientChannel(client, config.logChannelId)
-            if(channelLogin){
-                channelLogin.send(`<@${config.owner}>, Bot is online!`);
-                return true
-            }
-            return false
-        } catch (e){
-            log(`ERROR : Something went wrong when searching for the channel to send the online message : ${e}`)
-            return false
-        }
-    }
-    return false
-}
